ed1ec2e0b399a6621a4949a46a486d66178121fd
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-03-17 19:24:41 +0900

    chore: update deps

--- _dev_deps.ts
+++ _dev_deps.ts
@@ -2,6 +2,6 @@ export {
   assert,
   assertEquals,
   assertThrows,
-} from "https://deno.land/std@0.177.0/testing/asserts.ts";
-export { describe, it } from "https://deno.land/std@0.177.0/testing/bdd.ts";
+} from "https://deno.land/std@0.180.0/testing/asserts.ts";
+export { describe, it } from "https://deno.land/std@0.180.0/testing/bdd.ts";
 export { equalsResponse } from "https://deno.land/x/http_utils@1.0.0-beta.13/response.ts";
