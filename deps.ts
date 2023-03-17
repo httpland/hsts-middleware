f2d0d321cb449c1609dc0e29adf5d8b9a7b83f54
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-03-17 17:42:49 +0900

    refactor: remove unused

--- deps.ts
+++ deps.ts
@@ -3,9 +3,9 @@
 
 export { type Middleware } from "https://deno.land/x/http_middleware@1.0.0/mod.ts";
 export {
-  isNonNegativeInteger,
-  isString,
-} from "https://deno.land/x/isx@1.0.0-beta.24/mod.ts";
+  type StrictTransportSecurity,
+  stringify,
+} from "https://deno.land/x/hsts_parser@1.0.0-beta.1/mod.ts";
 
 export const enum SecurityHeaders {
   StrictTransportSecurity = "strict-transport-security",
